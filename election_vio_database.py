2cb08244a7ff2794e3e64612cd9f47307cf34dce
hgoers <52492407+hgoers@users.noreply.github.com>
2020-09-30 18:16:33 +1000

    Update script file

--- election_vio_database.py
+++ election_vio_database.py
@@ -5,6 +5,17 @@ Created on Wed Sep 30 16:18:23 2020
 @author: hgoer
 """
 
+# Import libraries
 import pandas as pd
+from clean_names import *
 
-df = pd.read_csv("https://raw.githubusercontent.com/hgoers/election_dates_database/master/Election_dates.csv")
+# Read in election dates data
+df_elections = pd.read_csv("https://raw.githubusercontent.com/hgoers/election_dates_database/master/Election_dates.csv")
+
+# Clean country names
+df_elections['country'] = clean_names(df_elections['country'])
+
+# Remove elections where no date has been set
+df_elections = df_elections[df_elections["date"]!="None"]
+df_elections["date"] = pd.to_datetime(df_elections["date"])
+
